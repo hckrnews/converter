6770426eaa2385325c593b1b0d72231280366aac
Pieter Wigboldus <w3n@w3news.org>
2025-06-20 15:30:19 +0200

    Auto merge package updates from renovate

--- src/file.js
+++ src/file.js
@@ -1,86 +1,86 @@
-import path from 'node:path';
-import { fileExists } from './fs.js';
+import path from 'node:path'
+import { fileExists } from './fs.js'
 
 /**
  * File model
  */
 class File {
-    /**
-     * Define the path
-     */
-    constructor() {
-        this.path = null;
+  /**
+   * Define the path
+   */
+  constructor () {
+    this.path = null
+  }
+
+  /**
+   * Set the path
+   * @param {string} filePath
+   */
+  setPath (filePath) {
+    if (!filePath || filePath.constructor !== String) {
+      throw new Error('File path should be a string')
     }
 
-    /**
-     * Set the path
-     * @param {string} filePath
-     */
-    setPath(filePath) {
-        if (!filePath || filePath.constructor !== String) {
-            throw new Error('File path should be a string');
-        }
-
-        if (!fileExists(filePath)) {
-            throw new Error('File path doesnt exists');
-        }
-
-        this.path = filePath;
+    if (!fileExists(filePath)) {
+      throw new Error('File path doesnt exists')
     }
 
-    /**
-     * Get the file info.
-     * @returns {object}
-     */
-    get info() {
-        return path.parse(this.path);
-    }
+    this.path = filePath
+  }
 
-    /**
-     * Get the file directory.
-     * @returns {string}
-     */
-    get directory() {
-        return this.info.dir;
-    }
+  /**
+   * Get the file info.
+   * @returns {object}
+   */
+  get info () {
+    return path.parse(this.path)
+  }
 
-    /**
-     * Get the file extension.
-     * @returns {string}
-     */
-    get extension() {
-        return this.info.ext;
-    }
+  /**
+   * Get the file directory.
+   * @returns {string}
+   */
+  get directory () {
+    return this.info.dir
+  }
 
-    /**
-     * Get the file name.
-     * @returns {string}
-     */
-    get base() {
-        return this.info.base;
-    }
+  /**
+   * Get the file extension.
+   * @returns {string}
+   */
+  get extension () {
+    return this.info.ext
+  }
 
-    /**
-     * Get the file name.
-     * @returns {string}
-     */
-    get name() {
-        return this.info.name;
-    }
+  /**
+   * Get the file name.
+   * @returns {string}
+   */
+  get base () {
+    return this.info.base
+  }
 
-    /**
-     * Create a file model
-     * @param {object} params
-     * @param {string} params.filePath
-     * @returns {object}
-     */
-    static create({ filePath }) {
-        const file = new File();
+  /**
+   * Get the file name.
+   * @returns {string}
+   */
+  get name () {
+    return this.info.name
+  }
 
-        file.setPath(filePath);
+  /**
+   * Create a file model
+   * @param {object} params
+   * @param {string} params.filePath
+   * @returns {object}
+   */
+  static create ({ filePath }) {
+    const file = new File()
 
-        return file;
-    }
+    file.setPath(filePath)
+
+    return file
+  }
 }
 
-export default File;
+export default File
--- src/fs.js
+++ src/fs.js
@@ -1,38 +1,38 @@
-import fs from 'node:fs';
+import fs from 'node:fs'
 
-const exists = (path) => fs.existsSync(path);
+const exists = (path) => fs.existsSync(path)
 
 const folderExists = (path) => {
-    if (!exists(path)) {
-        return false;
-    }
+  if (!exists(path)) {
+    return false
+  }
 
-    return fs.statSync(path).isDirectory();
-};
+  return fs.statSync(path).isDirectory()
+}
 
 const fileExists = (path) => {
-    if (!exists(path)) {
-        return false;
-    }
+  if (!exists(path)) {
+    return false
+  }
 
-    return fs.statSync(path).isFile();
-};
+  return fs.statSync(path).isFile()
+}
 
-const getFileName = (path) => path.split('/').pop();
+const getFileName = (path) => path.split('/').pop()
 
 const copyFile = (from, to) => {
-    if (!fileExists(from)) {
-        return false;
-    }
+  if (!fileExists(from)) {
+    return false
+  }
 
-    return fs.copyFileSync(from, to);
-};
+  return fs.copyFileSync(from, to)
+}
 const deleteFile = (path) => {
-    if (!fileExists(path)) {
-        return false;
-    }
+  if (!fileExists(path)) {
+    return false
+  }
 
-    return fs.unlinkSync(path);
-};
+  return fs.unlinkSync(path)
+}
 
-export { exists, folderExists, fileExists, getFileName, deleteFile, copyFile };
+export { exists, folderExists, fileExists, getFileName, deleteFile, copyFile }
